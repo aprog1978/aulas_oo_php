1ed0b754ada89d60e999c9eb2053b9913b4420b2
Francisco Analista Programador <aprog1978@yahoo.com>
2020-05-25 14:31:39 -0300

    aula teorica #10b conclusão

--- ProjetoPessoas/index.php
+++ ProjetoPessoas/index.php
@@ -15,9 +15,47 @@ and open the template in the editor.
         ?>
     </head>
     <body>
+        <pre>
         <?php
         // put your code here
         //Programa principal
+        require_once './Pessoa.php';
+        require_once './Aluno.php';
+        require_once './Professor.php';
+        require_once './Funcionario.php';
+        
+        $p1 = new Pessoa();
+        $p2= new Aluno();
+        $p3=new Professor();
+        $p4=new Funcionario();
+        
+        $p1->setNome("Pedro");
+        $p2->setNome("Maria");
+        $p3->setNome("Claudio");
+        $p4->setNome("Faniana");
+        
+        $p1->setSexo("M");
+        $p4->setSexo("F");
+        
+        $p2->setCurso("Informatica");
+        $p3->setSalario(2500.75);
+        $p4->setSetor("Estoque");
+        
+        $p3->receberAumento(550.20);
+        $p4->mudarTrabalho();
+        $p2->cancelarMatricula();
+                
+        echo '<br>';
+        print_r($p1);
+        echo '<hr>';
+        print_r($p2);
+        echo '<hr>';
+        print_r($p3);
+        echo '<hr>';
+        print_r($p4);
+        echo '<hr>';
+        
         ?>
+        </pre>
     </body>
 </html>
